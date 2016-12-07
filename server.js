43ec46a47a9d22c3fed05586f2b4bc84e31a7b7f
WATERYMEL0N <wateryymel0n@gmail.com>
2016-12-07 20:08:39 +1300

    Update server.js

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ var index = fs.readFileSync('/public/index.HTML').toString();
 
 http.createServer((req, res) => {
   if (req.headers['x-forward-proto'] != 'https') {
-    res.writeHead(302, ({
-      'Location': req.headers.host + '/'
+    res.writeHead(302, {
+      'Location': 'https:\\' + req.headers.host + '/'
     });
     res.end();
   } else if (req.url.indexOf('?') > -1) {
-    res.writeHead(302, ({
-      'Location': req.headers.host + '/'
+    res.writeHead(302, {
+      'Location': 'https:\\' + req.headers.host + '/'
     });
     res.end();
   } else if (req.url == '/') {
     red.end(index);
   } else {
-    res.writeHead(302, ({
-      'Location': req.headers.host + '/'
+    res.writeHead(302, {
+      'Location': 'https:\\' + req.headers.host + '/'
     });
     res.end();
   }
