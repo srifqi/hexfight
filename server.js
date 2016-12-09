aea194ef3c1eaf7b66988e8bfcc33510267eabad
WATERYMEL0N <wateryymel0n@gmail.com>
2016-12-09 18:31:05 +1300

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,5 @@
-var http = require('http'),
-    net  = require('net'),
-    path = require('path');
-    fs   = require('fs');
+var net = require('net');
 
-var index = fs.readFileSync(path.join(__dirname, 'public/index.html')).toString();
-
-var core = fs.readFileSync(path.join(__dirname, 'public/core.js')).toString();
-
-http.createServer((req, res) => {
-  if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] == 'http') {
-    res.writeHead(302, {
-      'Location': 'https:\\' + req.headers.host + '/'
-    });
-    res.end();
-  } else if (req.url.indexOf('?') > -1) {
-    res.writeHead(302, {
-      'Location': 'https:\\' + req.headers.host + '/'
-    });
-    res.end();
-  } else if (req.url == '/') {
-    res.end(index);
-  } else if (req.url == '/core.js') {
-    res.end(core);
-  } else {
-    res.writeHead(302, {
-      'Location': 'https:\\' + req.headers.host + '/'
-    });
-    res.end();
-  }
-}).listen(process.env.PORT);
+net.createServer((socket) => {
+  console.log('connection established');
+});
