b9161e87222a4f8be9bfbd3bebcc74f2ff3bb1c1
WATERYMEL0N <wateryymel0n@gmail.com>
2016-12-10 14:20:43 +1300

    Update core.js

--- public/core.js
+++ public/core.js
@@ -1,19 +1,39 @@
 var core = {
-  menu : {
+  menu: {
     
   },
-  rendering : {
+  rend: {
     
   },
-  ws : {
-    
+  webs: {
+    ws: new WebSocket(location.origin.replace('http', 'ws')),
+    sync: function(id) {
+      switch(id) {
+        case 0:
+          
+          break;
+      }
+    }
   },
-  main : {
-
+  plrs: {
+    add: function(id, n, t, s, h, x, y, d) {
+      
+    },
+    cache: function(id) {
+      
+    },
+    del: function(id) {
+      
+    }
+  },
+  main: {
+    init: function() {
+      core.menu.init();
+      core.rend.init();
+      core.webs.init();
+    },
+    start: function(n, t) {
+      core.webs.start(n, t);
+    }
   }
 };
-
-alert('&');
-
-
-//var ws = new WebSocket(location.origin.replace('http', 'ws'));
