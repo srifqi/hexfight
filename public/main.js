bed4e0177d9a2f1434d0af4f1623337b0aba9899
Muhammad Rifqi Priyo Susanto <muhammadrifqipriyosusanto@gmail.com>
2016-12-19 01:14:49 +0700

    A working (yet laggy) multiplayer
    Server prediction (still buggy).
    Using node.js modules: Express and Socket.IO.
    Ask player's nickname on page load.
    Tell player if the game is over.

--- public/main.js
+++ public/main.js
@@ -1,3 +1,9 @@
+var socket = io.connect('//localhost');
+
+// ///////////////////
+// The Game Engine //
+// ///////////////////
+
 const TYPE_PLAYER = 1;
 const TYPE_FOOD = 2;
 
@@ -25,16 +31,17 @@ Object2D.prototype.die = function () {
 };
 
 Object2D.prototype.draw = function () {
-	this.drawPoints += (this.points - this.drawPoints) * .5;
+	// this.drawPoints += (this.points - this.drawPoints) * .5;
+	this.drawPoints = this.points;
 };
 
-var Player = function (name) {
+var Player = function (name, element) {
 	Object2D.call(this);
 
 	this.name = name || 'guest';
 	this.type = TYPE_PLAYER;
 
-	this.element = null;
+	this.element = element || null;
 	this.powers = {};
 	this.points = 10;
 
@@ -79,6 +86,17 @@ Player.prototype.draw = function (ctx, isOther) {
 	ctx.fillText(this.name, 0, 0);
 	ctx.fillText(this.points, 0, this.points / 2);
 };
+Player.prototype.copy = function (obj) {
+	this.name = obj.name || 'guest';
+	// this.type = TYPE_PLAYER; // No need to check type.
+
+	this.element = obj.element;
+	this.powers = obj.powers;
+	this.points = obj.points;
+
+	this.pos.copy(obj.pos);
+	this.dir.copy(obj.dir);
+};
 
 var Food = function () {
 	Object2D.call(this);
@@ -89,8 +107,6 @@ var Food = function () {
 	this.points = 1;
 	this.pos = new v2();
 };
-
-Food.constructor = Food;
 
 Food.prototype = Object.create(Object2D.prototype);
 Food.constructor = Food;
@@ -111,32 +127,34 @@ Food.prototype.draw = function (ctx) {
 	ctx.strokeStyle = 'hsl(' + hue + ',100%,50%)';
 	ctx.stroke();
 };
+Food.prototype.copy = function (obj) {
+	this.name = obj.name;
+	// this.type = TYPE_FOOD; // No need to check type.
+	this.element = obj.element;
+	this.points = obj.points;
+	this.pos.copy(obj.pos);
+};
 
 var World = function (id) {
 	this.id = id || 1;
+	this.ids = [];
 	this.children = [];
 };
 
 World.prototype = {
-	add: function (player) {
+	add: function (ids, player) {
+		this.ids.push(ids);
 		this.children.push(player);
 
 		return this;
-	},
-	placeRandomFood: function () {
-		var food = new Food();
-		food.element = ELEMENTS[Math.round(Math.random() * 5)];
-		food.pos.add(new v2().set(
-			Math.round(Math.random() * 1000 - 500),
-			Math.round(Math.random() * 1000 - 500)
-		));
-		food.points = Math.round(Math.random() * 1 + 1);
-		this.children.push(food);
 	}
 };
 
 var Game = function () {
 	this.world = new World();
+
+	this.game_over = false;
+	this.game_update = null;
 
 	this.canvas = null;
 	this.ctx = null;
@@ -147,9 +165,9 @@ var Game = function () {
 };
 
 Game.prototype = {
-	start: function (canvas, name) {
-		this.player = new Player(name);
-		this.world.add(this.player);
+	start: function (canvas, name, element) {
+		this.player = new Player(name, element);
+		this.world.add(socket.id, this.player);
 		this.canvas = canvas;
 		this.canvas.scale = 1;
 		this.ctx = this.canvas.getContext('2d');
@@ -163,24 +181,35 @@ Game.prototype = {
 
 		this.canvas.addEventListener('mousemove', (function (that) {
 			return function (ev) {
+				if (that.game_over) return;
+
 				var x = ev.clientX - that.width / 2;
 				var y = -(ev.clientY - that.height / 2);
 				if (x * x + y * y <= that.player.points * that.player.points * that.canvas.scale * that.canvas.scale) {
 					that.player.steer(0, 0);
-					return;
+				} else {
+					var angle = Math.atan(y / x);
+					if (y < 0 && x < 0) {
+						angle += Math.PI;
+					} else if (y >= 0 && x < 0) {
+						angle += Math.PI;
+					} else if (y < 0 && x >= 0) {
+						angle += 2 * Math.PI;
+					}
+					that.player.steer(angle, Math.log10(that.player.points) * 2);
 				}
 
-				var angle = Math.atan(y / x);
-				if (y < 0 && x < 0) {
-					angle += Math.PI;
-				} else if (y >= 0 && x < 0) {
-					angle += Math.PI;
-				} else if (y < 0 && x >= 0) {
-					angle += 2 * Math.PI;
-				}
-				that.player.steer(angle, Math.log10(that.player.points) * 2);
+				socket.emit('update', that.player);
 			};
 		})(this));
+
+		socket.emit('init', this.player);
+
+		this.game_update = setInterval((function (that) {
+			return function (ev) {
+				that.update();
+			};
+		})(this), 1000 / 60);
 
 		this.render();
 
@@ -196,23 +225,39 @@ Game.prototype = {
 				var dist = ii.pos.distanceSq(jj.pos);
 				if (ii.points > jj.points && ii.points * ii.points >= dist) {
 					this.world.children.splice(j, 1);
-					if (ii instanceof Player) ii.points += jj.points;
+					if (ii.type === TYPE_PLAYER) ii.points += jj.points;
 					jj.die();
 				} else if (ii.points < jj.points && jj.points * jj.points >= dist) {
 					this.world.children.splice(i, 1);
-					if (jj instanceof Player) jj.points += ii.points;
+					if (jj.type === TYPE_PLAYER) jj.points += ii.points;
 					ii.die();
 				}
 			}
 		}
 	},
-	render: function () {
+	update: function () {
+		if (this.player.points === 0) {
+			this.game_over = true;
+			clearInterval(this.game_update);
+
+			socket.emit('dies', this.player);
+			alert('Game over!');
+
+			this.ctx.clearRect(0, 0, this.width, this.height);
+
+			return;
+		}
 		this.player.move();
 		for (var i = 1; i < this.world.children.length; i++) {
-			if (this.world.children[i] instanceof Player) {
+			if (this.world.children[i].type === TYPE_PLAYER) {
 				this.world.children[i].move();
 			}
 		}
+
+		this.checkCollision();
+	},
+	render: function () {
+		if (this.game_over) return;
 
 		this.ctx.clearRect(0, 0, this.width, this.height);
 		this.canvas.targetScale = .2 * this.height / this.player.points;
@@ -230,10 +275,6 @@ Game.prototype = {
 		}
 
 		this.ctx.setTransform(1, 0, 0, 1, 0, 0);
-
-		if (Math.random() > 0.9) this.world.placeRandomFood();
-
-		this.checkCollision();
 
 		this.ctx.font = '32px monospace';
 		this.ctx.fillText(this.world.children.length, 50, 50);
@@ -311,3 +352,37 @@ v2.prototype = {
 		return that;
 	}
 };
+
+// ////////////////////
+// The Client Cycle //
+// ////////////////////
+
+var game;
+
+socket.on('connect', function () {
+	game = new Game();
+	game.start(c, prompt('Enter nickname:'));
+});
+
+socket.on('world_update', function (data) {
+	game.world.ids.splice(1);
+	game.world.children.splice(1);
+	var ids = data.ids;
+	var c = data.children;
+	for (var i = 0; i < c.length; i++) {
+		if (ids[i] === socket.id) {
+			game.world.children[0].copy(c[i]);
+			continue;
+		}
+
+		if (c[i].type === TYPE_PLAYER) {
+			var player = new Player();
+			player.copy(c[i]);
+			game.world.children.push(player);
+		} else if (c[i].type === TYPE_FOOD) {
+			var food = new Food();
+			food.copy(c[i]);
+			game.world.children.push(food);
+		}
+	}
+});
--- server.js
+++ server.js
@@ -1,9 +1,279 @@
+// Start the server.
 const express = require('express'),
 	PORT = process.env.PORT || 80;
 
 var app = express();
-
 var server = app.listen(PORT);
+console.log('Listening on PORT ' + PORT);
 
 app.use(express.static('public'));
-console.log('Listening on PORT ' + PORT);
+
+// Initialize WebSocket (using Socket.IO).
+var io = require('socket.io')(server);
+
+// ///////////////////
+// The Game Engine //
+// ///////////////////
+
+const TYPE_PLAYER = 1;
+const TYPE_FOOD = 2;
+
+const ELEMENT_1 = 1;
+const ELEMENT_2 = 2;
+const ELEMENT_3 = 4;
+const ELEMENT_4 = 8;
+const ELEMENT_5 = 16;
+const ELEMENT_6 = 32;
+const ELEMENTS = [
+	ELEMENT_1, ELEMENT_2, ELEMENT_3, ELEMENT_4, ELEMENT_5, ELEMENT_6
+];
+
+var Object2D = function (name) {
+	this.name = name || 'object';
+	this.type = null;
+	this.points = 0;
+	this.pos = new v2();
+};
+
+Object2D.prototype.die = function () {
+	this.points = 0;
+};
+
+var Player = function (name, element) {
+	Object2D.call(this);
+
+	this.name = name || 'guest';
+	this.type = TYPE_PLAYER;
+
+	this.element = null;
+	this.powers = {};
+	this.points = 10;
+
+	this.pos = new v2();
+	this.dir = new v2();
+};
+
+Player.prototype = Object.create(Object2D.prototype);
+Player.constructor = Player;
+
+Player.prototype.steer = function (angle, speed) {
+	var x = speed * Math.cos(angle);
+	var y = speed * Math.sin(angle);
+
+	this.dir.set(x, y);
+};
+Player.prototype.move = function () {
+	var force = this.dir.clone();
+	force.multiplyScalar(.3);
+	this.pos.add(force);
+};
+Player.prototype.copy = function (obj) {
+	this.name = obj.name || 'guest';
+	// this.type = TYPE_PLAYER; // No need to check type.
+
+	this.element = obj.element;
+	this.powers = obj.powers;
+	this.points = obj.points;
+
+	this.pos.copy(obj.pos);
+	this.dir.copy(obj.dir);
+};
+
+var Food = function () {
+	Object2D.call(this);
+
+	this.name = 'food';
+	this.type = TYPE_FOOD;
+	this.element = null;
+	this.points = 1;
+	this.pos = new v2();
+};
+
+Food.prototype = Object.create(Object2D.prototype);
+Food.constructor = Food;
+
+Food.prototype.copy = function (obj) {
+	this.name = obj.name;
+	// this.type = TYPE_FOOD; // No need to check type.
+	this.element = obj.element;
+	this.points = obj.points;
+	this.pos.copy(obj.pos);
+};
+
+var World = function (id) {
+	this.id = id || 1;
+	this.ids = [];
+	this.children = [];
+};
+
+World.prototype = {
+	add: function (ids, player) {
+		this.ids.push(ids);
+		this.children.push(player);
+
+		return this;
+	},
+	placeRandomFood: function () {
+		var food = new Food();
+		food.element = ELEMENTS[Math.round(Math.random() * 5)];
+		food.pos.add(new v2().set(
+			Math.round(Math.random() * 1000 - 500),
+			Math.round(Math.random() * 1000 - 500)
+		));
+		food.points = Math.round(Math.random() * 1 + 1);
+		this.add(this.children.length + 1, food);
+	}
+};
+
+var Game = function () {
+	this.world = new World();
+	this.last_food = -1;
+};
+
+Game.prototype = {
+	checkCollision: function () {
+		if (this.world.children.length < 2) return;
+		for (var i = 0; i < this.world.children.length; i++) {
+			var ii = this.world.children[i];
+			if (ii == undefined) continue;
+			for (var j = i + 1; j < this.world.children.length; j++) {
+				var jj = this.world.children[j];
+				if (jj == undefined) continue;
+				var dist = ii.pos.distanceSq(jj.pos);
+				if (ii.points > jj.points && ii.points * ii.points >= dist) {
+					this.world.ids.splice(j, 1);
+					this.world.children.splice(j, 1);
+					if (ii.type === TYPE_PLAYER) ii.points += jj.points;
+					jj.die();
+				} else if (ii.points < jj.points && jj.points * jj.points >= dist) {
+					this.world.ids.splice(i, 1);
+					this.world.children.splice(i, 1);
+					if (jj.type === TYPE_PLAYER) jj.points += ii.points;
+					ii.die();
+				}
+			}
+		}
+	},
+	update: function () {
+		for (var i = 0; i < this.world.children.length; i++) {
+			if (this.world.children[i].type === TYPE_PLAYER) {
+				this.world.children[i].move();
+			}
+		}
+
+		if (Date.now() - this.last_food > 100 && Math.random() <= 0.2) {
+			this.world.placeRandomFood();
+			this.last_food = Date.now();
+		}
+
+		// console.log(this.world.children.length);
+
+		this.checkCollision();
+	}
+};
+
+var v2 = function (x, y) {
+	this.x = x || 0;
+	this.y = y || 0;
+};
+
+v2.prototype = {
+	set: function (x, y) {
+		this.x = x;
+		this.y = y;
+
+		return this;
+	},
+	copy: function (v) {
+		this.x = v.x;
+		this.y = v.y;
+
+		return this;
+	},
+	add: function (v) {
+		this.x += v.x;
+		this.y += v.y;
+
+		return this;
+	},
+	sub: function (v) {
+		this.x -= v.x;
+		this.y -= v.y;
+
+		return this;
+	},
+	multiply: function (v) {
+		this.x *= v.x;
+		this.y *= v.y;
+
+		return this;
+	},
+	multiplyScalar: function (s) {
+		this.x *= s;
+		this.y *= s;
+
+		return this;
+	},
+	distanceSq: function (v2) {
+		var target = v2.clone();
+		target.sub(this);
+		return target.x * target.x + target.y * target.y;
+	},
+	distance: function (v2) {
+		return Math.sqrt(this.distanceSq(v2));
+	},
+	clone: function () {
+		var that = new v2();
+		that.x = this.x;
+		that.y = this.y;
+
+		return that;
+	}
+};
+
+// ////////////////////
+// The Server Cycle //
+// ////////////////////
+
+var game = new Game();
+
+// Run the game.
+// (60 FPS)
+function RUN () {
+	game.update();
+}
+setInterval(RUN, 1000 / 60);
+
+// Broadcast to players about all players in this game.
+// (30 FPS)
+function BC () {
+	io.sockets.emit('world_update', game.world);
+}
+setInterval(BC, 1000 / 30);
+
+// User I/O.
+io.sockets.on('connection', function (socket) {
+	console.log('New player joins the game: ' + socket.id + '.');
+
+	socket.on('init', function (data) {
+		var player = new Player(data.name, data.element);
+		game.world.add(socket.id, player);
+	});
+
+	socket.on('update', function (data) {
+		var id = game.world.ids.indexOf(socket.id);
+		if (id < 0) return;
+		game.world.children[id].copy(data);
+	});
+
+	socket.on('dies', function (data) {
+		console.log('A player dies: ' + socket.id + '.');
+	});
+
+	socket.on('disconnect', function () {
+		var id = game.world.ids.indexOf(socket.id);
+		if (id < 0) return;
+		game.world.children[id].die();
+		console.log('A player lefts the game: ' + socket.id + '.');
+	});
+});
